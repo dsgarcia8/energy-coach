bf18dd67368f018348e87478717aeb4f7efe52df
Daniela Garcia <dsgarcia@espol.edu.ec>
2021-11-02 16:11:04 -0500

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import AppNavigator from './src';
+
+const App = () => {
+  return (
+    <AppNavigator />
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AppNavigator = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default AppNavigator;
