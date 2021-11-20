76942ea77ad2a7ac27e4f8b2cb78548138f8be56
Daniela Garcia <dsgarcia@espol.edu.ec>
2021-11-19 22:54:27 -0500

    feat: profile screen

--- src/containers/ProfileScreen.js
+++ src/containers/ProfileScreen.js
@@ -1,7 +1,16 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {Button, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
 import {firebase} from '@react-native-firebase/auth';
-
+import FormButton from '../components/FormButton';
+const screenWidth = Dimensions.get('window').width;
 const ProfileScreen = () => {
   return (
     <View
@@ -12,89 +21,88 @@ const ProfileScreen = () => {
         width: '100%',
         height: '100%',
       }}>
-      <View style={{justifyContent: 'flex-end'}}>
-
-        <Button
-          title="Log out"
-          onPress={async () => {
-            // TODO: Validar que los campos esten llenos
-            // TODO: Autenticacion
-            await firebase.auth().signOut();
+      <View style={styles.container}>
+        <View style={styles.header} />
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: 'https://cdn2.vectorstock.com/i/1000x1000/20/76/man-avatar-profile-vector-21372076.jpg',
           }}
         />
+        <View style={styles.body}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.name}>John Doe</Text>
+            <Text style={styles.info}>Administrador</Text>
+            <Text style={styles.description}>
+              Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum
+              electram expetendis, omittam deseruisse consequuntur ius an,
+            </Text>
+            <FormButton
+              buttonTitle="Log Out"
+              onPress={async () => {
+                // TODO: Validar que los campos esten llenos
+                // TODO: Autenticacion
+                await firebase.auth().signOut();
+              }}
+            />
+          </View>
+        </View>
       </View>
-
     </View>
   );
 };
 
-const data1 = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-  datasets: [
-    {
-      data: [20, 45, 28, 80, 99, 43],
-      color: (opacity = 1) => `rgba(0, 64, 153, ${opacity})`, // optional
-      strokeWidth: 2, // optional
-    },
-  ],
-  legend: ['Energy Consumption'], // optional
-};
-
-// const styles = StyleSheet.create({
-//   header:{
-//     backgroundColor: "#00BFFF",
-//     height:200,
-//   },
-//   avatar: {
-//     width: 130,
-//     height: 130,
-//     borderRadius: 63,
-//     borderWidth: 4,
-//     borderColor: "white",
-//     marginBottom:10,
-//     alignSelf:'center',
-//     position: 'absolute',
-//     marginTop:130
-//   },
-//   name:{
-//     fontSize:22,
-//     color:"#FFFFFF",
-//     fontWeight:'600',
-//   },
-//   body:{
-//     marginTop:40,
-//   },
-//   bodyContent: {
-//     flex: 1,
-//     alignItems: 'center',
-//     padding:30,
-//   },
-//   name:{
-//     fontSize:28,
-//     color: "#696969",
-//     fontWeight: "600"
-//   },
-//   info:{
-//     fontSize:16,
-//     color: "#00BFFF",
-//     marginTop:10
-//   },
-//   description:{
-//     fontSize:16,
-//     color: "#696969",
-//     marginTop:10,
-//     textAlign: 'center'
-//   },
-//   buttonContainer: {
-//     marginTop:10,
-//     height:45,
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     marginBottom:20,
-//     width:250,
-//     borderRadius:30,
-//     backgroundColor: "#00BFFF",
-//   },
-// });
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#57c4ea',
+    height: 350,
+    width: screenWidth,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 280,
+  },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: '600',
+  },
+  info: {
+    fontSize: 16,
+    color: '#00BFFF',
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: '#696969',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#00BFFF',
+  },
+});
 export default ProfileScreen;
